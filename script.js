afa3dc43bae5ddab9a5ae5e5b757bfa4a159e2e8
Javeria-Ashraf <javeriaashraf5002@gmail.com>
2024-12-06 11:40:18 +0500

    Add different pages

---
+++ script.js
@@ -0,0 +1,6 @@
+//About me
+let showText = document.getElementById("showButton");
+showText.addEventListener('click',function(){
+   let textElement = document.getElementById("hiddenText");
+    textElement.style.display = "block";
+});
